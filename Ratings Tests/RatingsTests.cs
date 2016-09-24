dc8c3577b3fd94e466655188b5a853fc54148ed4
Elibrighton <eli.brighton@gmail.com>
2016-09-23 20:40:58 -0500

    Add Ratings and RatingsTests projects

---
+++ Ratings Tests/RatingsTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ratings_Tests
+{
+    [TestClass]
+    public class RatingsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
